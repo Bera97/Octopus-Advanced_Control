4f9382f9dd2758fe5c67ec11f2b11298a29db6bd
Berin Đikić <berin.ba@gmail.com>
2022-03-27 13:31:13 +0200

    Refactored the code to use matrix operations
    Ass_2 - correct
    Ass_1 - probably has a small bug

---
+++ Ass_1.m
@@ -0,0 +1,18 @@
+clear all;
+
+x_start = 0;
+y_start = 0;
+x_end = 2;
+y_end = 2;
+
+[XX, YY] = meshgrid(x_start:0.1:x_end,y_start:0.1:y_end);
+
+f = {@(x,y)-(6*x)./((1+x.^2).^2)+2*y; @(x,y)-2*(x+y)./((1+x.^2).^2)};
+
+U = f{1}(XX, YY);
+V = f{2}(XX, YY);
+
+figure(1)
+quiver(XX,YY,U,V, 'k', 'LineWidth',1.2)
+xlim([x_start-0.05 x_end+0.05])
+ylim([y_start-0.05 y_end+0.05])
---
+++ Ass_2.m
@@ -0,0 +1,18 @@
+clear all;
+
+x_start = 0;
+y_start = 0;
+x_end = 2;
+y_end = 2;
+
+[YY, XX] = meshgrid(x_start:0.1:x_end,y_start:0.1:y_end);
+
+f = {@(x,y)x.*(1-y); @(x,y)y.*(x-1)};
+
+U = f{1}(XX, YY);
+V = f{2}(XX, YY);
+
+figure(1)
+quiver(XX,YY,U,V, 'k', 'LineWidth',1.2)
+xlim([x_start-0.05 x_end+0.05])
+ylim([y_start-0.05 y_end+0.05])
